64f61c699de83de1a366054da32eab697bb15a31
Ignacio Fernandez <nachoveiga1014@gmail.com>
2016-11-07 13:33:59 +0100

    dragula dependencies

--- firebase_PostIt/src/app/app.module.ts
+++ firebase_PostIt/src/app/app.module.ts
@@ -7,32 +7,35 @@ import { AppRoutingModule } from './router.component';
 import { AngularFireModule } from 'angularfire2';
 import { FormComponent } from './form.component';
 import { ListComponent } from './list.component';
+import { DragulaModule, DragulaService } from '../../node_modules/ng2-dragula/ng2-dragula';
+
 
 
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBXNhwkOT_iu_9HIktEw78YGCydAjs6RQ0",
-  authDomain: "angularintegration-3b520.firebaseapp.com",
-  databaseURL: "https://angularintegration-3b520.firebaseio.com",
-  storageBucket: "angularintegration-3b520.appspot.com",
-  messagingSenderId: "87154413034"
+    apiKey: "AIzaSyBXNhwkOT_iu_9HIktEw78YGCydAjs6RQ0",
+    authDomain: "angularintegration-3b520.firebaseapp.com",
+    databaseURL: "https://angularintegration-3b520.firebaseio.com",
+    storageBucket: "angularintegration-3b520.appspot.com",
+    messagingSenderId: "87154413034"
 };
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent,
-    ListComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AppRoutingModule
+    declarations: [
+        AppComponent,
+        FormComponent,
+        ListComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        DragulaModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AppRoutingModule
 
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- firebase_PostIt/src/main.ts
+++ firebase_PostIt/src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import 'dragula/dist/dragula.css';
+
 
 if (environment.production) {
   enableProdMode();
