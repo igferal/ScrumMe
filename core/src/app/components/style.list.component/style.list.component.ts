b1c2f43413f35067376690489a74bea17d7a5366
Ignacio Fernandez <nachoveiga1014@gmail.com>
2017-04-04 16:30:26 +0200

    style guide ampliada

--- core/src/app/components/style.list.component/style.list.component.ts
+++ core/src/app/components/style.list.component/style.list.component.ts
@@ -1,3 +1,4 @@
+import { PostIt } from './../../model/post.it';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
 })
 export class StyleListComponent {
 
+  msgs = [];
+  p1: PostIt;
+  p2: PostIt;
+  p3: PostIt;
+
+
+  constructor() {
+    this.msgs.push({ severity: 'error', summary: 'Error!', detail: 'Soy una alerta' });
+    this.p1 = new PostIt('titulo', 'contenido', 'dev', 10, '');
+    this.p2 = new PostIt('titulo', 'contenido', '', 10, '');
+    this.p2.workedHours = 5;
+    this.p3 = new PostIt('titulo', 'contenido', '', 10, '');
+    this.p3.closed = true;
+  }
 
 }
